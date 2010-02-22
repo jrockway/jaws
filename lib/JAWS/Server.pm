a249ba57a410777e2bc7af8b9fa55375b143bae0
Jonathan Rockway <jon@jrock.us>
2010-02-22 06:06:46 -0600

    add proxy

---
+++ lib/JAWS/Server.pm
@@ -0,0 +1,48 @@
+use MooseX::Declare;
+
+class JAWS::Server with Plack::Component::Role {
+    use AnyEvent;
+    use Coro;
+    use AnyEvent::HTTP;
+    use URI;
+    use DDS;
+
+    method call(HashRef $env) {
+        my $url = $env->{PATH_INFO};
+        $url =~ s{^/}{};
+        $url = "http://$url" if $url !~ m{^\w+://};
+
+        my $uri = URI->new( $url );
+        die "invalid URI" unless $uri->scheme eq 'http';
+
+        my %req_headers = map {
+            my $k = $_;
+            $k =~ s/^HTTP_//;
+            $k =~ tr/_/-/;
+            lc $k => $env->{$_} }
+          grep { /^HTTP/ } keys %$env;
+
+        my $body_cb = Coro::rouse_cb;
+        http_get "$uri", want_body_handle => 1, headers => \%req_headers, $body_cb;
+
+        return sub {
+            my $respond = shift;
+
+            my ($body_fh, $headers) = Coro::rouse_wait($body_cb);
+            if($headers->{Status} >= 590){ # internal error
+                $respond->([ $headers->{Status}, [], [ $headers->{Reason} ] ]);
+                return;
+            }
+
+            my $writer = $respond->([ 200, [ %$headers ]]);
+
+            my $done = Coro::rouse_cb;
+            $body_fh->on_error(sub { $writer->close; $done->(); });
+            $body_fh->on_eof(sub { $writer->close; $done->(); });
+            $body_fh->on_read(sub { $writer->write( delete $body_fh->{rbuf} ) });
+
+            Coro::rouse_wait($done);
+            $body_fh->destroy;
+        };
+    }
+}
